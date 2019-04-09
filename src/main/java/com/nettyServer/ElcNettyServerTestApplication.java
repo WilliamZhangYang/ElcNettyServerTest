4ddfbc5ca73ca3f57f027f945cfab4f483a8cd3c
WilliamZhangYang <312508762@qq.com>
2019-04-09 21:20:33 +0800

    tcp netty server

---
+++ src/main/java/com/nettyServer/ElcNettyServerTestApplication.java
@@ -0,0 +1,13 @@
+package com.nettyServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElcNettyServerTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElcNettyServerTestApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/nettyServer/common/NettyServerListener.java
@@ -0,0 +1,45 @@
+package com.nettyServer.common;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * netty服务监听器
+ * @author hdnav
+ *
+ */
+@WebListener
+public class NettyServerListener implements ServletContextListener {
+
+	/**
+	 * 注入NettyServer
+	 */
+	@Autowired
+    private NettyServer nettyServer;
+	
+	@Override
+    public void contextInitialized(ServletContextEvent sce) {
+        Thread thread = new Thread(new NettyServerThread());
+        // 启动netty服务
+        thread.start();
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+
+    }
+    
+    /**
+     * netty服务启动线程
+     */
+    private class NettyServerThread implements Runnable {
+
+        @Override
+        public void run() {
+            nettyServer.run();
+        }
+    }
+}
